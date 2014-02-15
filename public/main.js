34a77420d9f5b2803e4741551e29ea2d42e761db
Kirby Kohlmorgen <kirby@kkohlmorgen.com>
2014-02-14 23:03:54 -0500

    v1.0

--- public/main.js
+++ public/main.js
@@ -1,9 +1,9 @@
-// window.setTimeout(function(){location.reload()},1000)
+window.setTimeout(function(){location.reload()},5000)
 
-  var reset = function(){
-    location.reload();
-    $.ajax({
-      type: "GET",
-    url: "/reset",
-    });
-  }
+var reset = function(){
+  location.reload();
+  $.ajax({
+    type: "GET",
+    url: "/reset"
+  });
+}
